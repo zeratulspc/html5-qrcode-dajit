d923530fbfbc11ce18c8d8ce93017221d6299e5c
zeratulspc <zeratulspc@gmail.com>
2025-04-15 10:35:19 +0900

    번역

--- src/strings.ts
+++ src/strings.ts
@@ -13,37 +13,39 @@
  * 
  * TODO(mebjas): Support internalization.
  */
+
+/**
+ * Modifyed By zeratulspc 2025-04-15
+ * translated to Korean
+ */
 export class Html5QrcodeStrings {
 
     public static codeParseError(exception: any): string {
-        return `QR code parse error, error = ${exception}`;
+        return `QR 코드 파싱 오류, 오류 = ${exception}`;
     }
 
     public static errorGettingUserMedia(error: any): string {
-        return `Error getting userMedia, error = ${error}`;
+        return `userMedia 가져오기 오류, 오류 = ${error}`;
     }
 
     public static onlyDeviceSupportedError(): string {
-        return "The device doesn't support navigator.mediaDevices , only "
-        + "supported cameraIdOrConfig in this case is deviceId parameter "
-        + "(string).";
+        return "이 장치는 navigator.mediaDevices를 지원하지 않습니다. 이 경우 지원되는 cameraIdOrConfig는 deviceId 매개변수(문자열)뿐입니다.";
     }
 
     public static cameraStreamingNotSupported(): string {
-        return "Camera streaming not supported by the browser.";
+        return "브라우저에서 카메라 스트리밍을 지원하지 않습니다.";
     }
 
     public static unableToQuerySupportedDevices(): string {
-        return "Unable to query supported devices, unknown error.";
+        return "지원되는 장치를 쿼리할 수 없습니다. 알 수 없는 오류입니다.";
     }
 
     public static insecureContextCameraQueryError(): string {
-        return "Camera access is only supported in secure context like https "
-        + "or localhost.";
+        return "카메라 액세스는 https 또는 localhost와 같은 보안 컨텍스트에서만 지원됩니다.";
     }
 
     public static scannerPaused(): string {
-        return "Scanner paused";
+        return "스캐너가 일시 중지되었습니다.";
     }
 }
 
@@ -55,71 +57,71 @@ export class Html5QrcodeStrings {
 export class Html5QrcodeScannerStrings {
 
     public static scanningStatus(): string {
-        return "Scanning";
+        return "스캔 중";
     }
 
     public static idleStatus(): string {
-        return "Idle";
+        return "대기 중";
     }
 
     public static errorStatus(): string {
-        return "Error";
+        return "오류";
     }
 
     public static permissionStatus(): string {
-        return "Permission";
+        return "권한";
     }
 
     public static noCameraFoundErrorStatus(): string {
-        return "No Cameras";
+        return "카메라를 찾을 수 없음";
     }
 
     public static lastMatch(decodedText: string): string {
-        return `Last Match: ${decodedText}`;
+        return `마지막 매칭: ${decodedText}`;
     }
 
     public static codeScannerTitle(): string {
-        return "Code Scanner";
+        return "코드 스캐너";
     }
 
     public static cameraPermissionTitle(): string {
-        return "Request Camera Permissions";
+        return "카메라 권한 요청";
     }
 
     public static cameraPermissionRequesting(): string {
-        return "Requesting camera permissions...";
+        return "카메라 권한 요청 중...";
     }
 
     public static noCameraFound(): string {
-        return "No camera found";
+        return "카메라를 찾을 수 없습니다.";
     }
 
     public static scanButtonStopScanningText(): string {
-        return "Stop Scanning";
+        return "스캔 중지";
     }
 
     public static scanButtonStartScanningText(): string {
-        return "Start Scanning";
+        return "스캔 시작";
     }
 
     public static torchOnButton(): string {
-        return "Switch On Torch";
+        return "손전등 켜기";
     }
 
     public static torchOffButton(): string {
-        return "Switch Off Torch";
+        return "손전등 끄기";
     }
 
     public static torchOnFailedMessage(): string {
-        return "Failed to turn on torch";
+        return "손전등을 켜는 데 실패했습니다.";
     }
 
     public static torchOffFailedMessage(): string {
-        return "Failed to turn off torch";
+        return "손전등을 끄는 데 실패했습니다.";
     }
 
     public static scanButtonScanningStarting(): string {
-        return "Launching Camera...";
+        return "카메라 실행 중";
     }
 
     /**
@@ -128,7 +130,7 @@ export class Html5QrcodeScannerStrings {
      * This will be used to switch to file based scanning.
      */
     public static textIfCameraScanSelected(): string {
-        return "Scan an Image File";
+        return "이미지 파일 스캔";
     }
 
     /**
@@ -137,53 +139,53 @@ export class Html5QrcodeScannerStrings {
      * This will be used to switch to camera based scanning.
      */
     public static textIfFileScanSelected(): string {
-        return "Scan using camera directly";
+        return "카메라를 사용하여 직접 스캔";
     }
 
     public static selectCamera(): string {
-        return "Select Camera";
+        return "카메라 선택";
     }
 
     public static fileSelectionChooseImage(): string {
-        return "Choose Image";
+        return "이미지 선택";
     }
 
     public static fileSelectionChooseAnother(): string {
-        return "Choose Another";
+        return "다른 이미지 선택";
     }
 
     public static fileSelectionNoImageSelected(): string {
-        return "No image choosen";
+        return "선택된 이미지 없음";
     }
 
     /** Prefix to be given to anonymous cameras. */
     public static anonymousCameraPrefix(): string {
-        return "Anonymous Camera";
+        return "익명 카메라";
     }
 
     public static dragAndDropMessage(): string {
-        return "Or drop an image to scan";
+        return "또는 이미지를 드롭하여 스캔";
     }
 
     public static dragAndDropMessageOnlyImages(): string {
-        return "Or drop an image to scan (other files not supported)";
+        return "또는 이미지를 드롭하여 스캔 (다른 파일은 지원되지 않음)";
     }
 
     /** Value for zoom. */
     public static zoom(): string {
-        return "zoom";
+        return "줌";
     }
 
     public static loadingImage(): string {
-        return "Loading image...";
+        return "이미지 로딩 중...";
     }
 
     public static cameraScanAltText(): string {
-        return "Camera based scan";
+        return "카메라 기반 스캔";
     }
 
     public static fileScanAltText(): string {
-        return "Fule based scan";
+        return "파일 기반 스캔";
     }
 }
 
@@ -191,10 +193,10 @@ export class Html5QrcodeScannerStrings {
 export class LibraryInfoStrings {
 
     public static poweredBy(): string {
-        return "Powered by ";
+        return "제공: ";
     }
 
     public static reportIssues(): string {
-        return "Report issues";
+        return "문제 보고";
     }
 }
